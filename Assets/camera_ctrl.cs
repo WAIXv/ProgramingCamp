d23f7980e4421ae135888f6564bf0eee35c76a8e
dfdyz <2649111464@qq.com>
2022-11-09 23:39:28 +0800

    11-9

--- Assets/camera_ctrl.cs
+++ Assets/camera_ctrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,6 +8,12 @@ public class camera_ctrl : MonoBehaviour
 {
     private Camera cam;
     private GameObject player;
+    private GameObject bg;
+
+    private GameObject danger_layer;
+    private GameObject lost_layer;
+
+    public float edge = 10f;
 
     static float a_x = 2.2f; 
     static float a_y = 1.9f;
@@ -16,11 +23,14 @@ public class camera_ctrl : MonoBehaviour
     {
         cam = gameObject.GetComponent<Camera>();
         player = GameObject.Find("player");
+        bg = GameObject.Find("bg");
+        danger_layer = GameObject.Find("danger");
+        lost_layer = GameObject.Find("lost");
     }
 
     // Update is called once per frame
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 cam_pos = cam.transform.position;
         Vector3 p_pos = player.transform.position;
@@ -52,11 +62,24 @@ public class camera_ctrl : MonoBehaviour
         if (cam_pos.y <= 0.8f) cam_pos.y = 0.8f;
 
         cam.transform.position = cam_pos;
-
     }
 
     void Update()
     {
-        
+        Color col = danger_layer.GetComponent<SpriteRenderer>().color;
+        if (player.transform.position.y < -5f)
+        {
+            col.a += Time.deltaTime * 1.5f;
+            col.a = math.min(1.0f,col.a);
+
+        }
+        else
+        {
+            col.a -= Time.deltaTime * 1.5f;
+            col.a = math.max(0.0f, col.a);
+        }
+
+        danger_layer.GetComponent<SpriteRenderer>().color = col;
+        lost_layer.GetComponent<SpriteRenderer>().color = col;
     }
 }
