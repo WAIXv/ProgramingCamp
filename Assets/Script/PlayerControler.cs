9e0f6136508e171373aeed0ee95b3574efbacc2f
ScReadyToPlay <2521463872@qq.com>
2022-11-12 11:26:08 +0800

    week1
    场景搭建，角色移动，角色动作

---
+++ Assets/Script/PlayerControler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpforce;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedirection = Input.GetAxisRaw("Horizontal");
+
+        //角色移动
+        if(horizontalmove != 0)
+        {
+            rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime,rb.velocity.y);
+            anim.SetFloat("running",Mathf.Abs(facedirection));
+        }
+
+        if(facedirection != 0)//控制角色朝向
+        {
+            transform.localScale = new Vector3(facedirection,1,1);
+        }
+        //角色跳跃
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpforce * Time.deltaTime);
+        }
+    }
+}
