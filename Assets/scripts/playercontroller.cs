dfbc1a1d8d2841d770e242958315dad54c9be402
harryzhen <2765161790@qq.com>
2022-11-13 22:28:40 +0800

    角色动画

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-    [SerializeField] private  Rigidbody2D rb;
+    [SerializeField] private  Rigidbody2D player;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Collider2D feet;
     [SerializeField] private float speedx;
     [SerializeField] private float jumpforce;
-    
+    [SerializeField] private LayerMask ground;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class playercontroller : MonoBehaviour
     void FixedUpdate()
     {
         movement();
+        SwitchAnim();
     }
 
     //角色移动
@@ -26,18 +30,46 @@ public class playercontroller : MonoBehaviour
         float horizontalmove = Input.GetAxis("Horizontal");
         float facedirection = Input.GetAxisRaw("Horizontal");
         
-
+        //角色移动
         if  (horizontalmove != 0)
         {
-            rb.velocity = new Vector2 (horizontalmove*speedx*Time.fixedDeltaTime, rb.velocity.y);
+            player.velocity = new Vector2 (horizontalmove*speedx*Time.fixedDeltaTime, player.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedirection));
         }
         if (facedirection != 0)
         {
             transform.localScale = new Vector3(facedirection, 1, 1);
         }
+
+        //角色跳跃
         if (Input.GetButton("Jump"))
         {
-            rb.velocity = new Vector2 (rb.velocity.x, jumpforce*Time.fixedDeltaTime);   
+            player.velocity = new Vector2 (player.velocity.x, jumpforce*Time.fixedDeltaTime);
+            anim.SetBool("jumping", true);
         }
     }
+
+    //变化动画
+    void SwitchAnim()
+    {
+        anim.SetBool("idle", false);
+
+        if(anim.GetBool("jumping"))
+        {
+            if(player.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if(feet.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idle",true);
+        }
+    }
+
+
+
+
 }
