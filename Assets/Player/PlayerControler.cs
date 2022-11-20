09c50c5a21f99b1a631973046dc524205d21257e
WutongtheSummerLeaves <860724709@qq.com>
2022-11-20 18:05:53 +0800

    New homework,what's up!

--- Assets/Player/PlayerControler.cs
+++ Assets/Player/PlayerControler.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControler : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator anim;
+
     public Collider2D coll;
     public float speed;
     public float jumpforce;
     public LayerMask ground;
+    public int Cherry;
+    public int Gem;
+
+    public Text CherryNum;
+    public Text GemNum;
+    private bool isHurt;//默认时false
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +29,10 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        if(!isHurt)
+        {
+            Movement();
+        }
         SwitchAnim();
     }
 
@@ -42,16 +54,30 @@ public class PlayerControler : MonoBehaviour
         }
 
         //角色跳跃
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
             anim.SetBool("jumping", true);
         }
+
+
+        if(Input.GetButtonDown("Vertical") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(horizontalmove * 0, rb.velocity.y);
+            anim.SetBool("crouching", true);
+            
+        }
+
     }
 
+    //切换动画效果
     void SwitchAnim()
     {
         anim.SetBool("idle", false);
+        if(rb.velocity.y<0.1f && !coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", true);
+        }
         if (anim.GetBool("jumping"))
         {
             if (rb.velocity.y < 0)
@@ -60,12 +86,81 @@ public class PlayerControler : MonoBehaviour
                 anim.SetBool("falling", true);
             }
         }
+        else if(isHurt)
+        {
+            anim.SetBool("hurt", true);
+            anim.SetFloat("running", 0);
+            if (Mathf.Abs(rb.velocity.x)<0.1f)
+            {
+                anim.SetBool("hurt", false);
+                anim.SetBool("idle", true);
+                isHurt = false;
+            }
+        }
         else if(coll.IsTouchingLayers(ground))
         {
             anim.SetBool("falling", false);
             anim.SetBool("idle", true);
         }
+
+        if (anim.GetBool("crouching"))
+        {
+
+            if(!Input.GetButtonDown("Vertical"))
+            {
+                anim.SetBool("crouching", false);
+                anim.SetBool("idle", true);
+            }
+
+        }
     }
 
+    //收集物品
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Collection")
+        {
+            
+            
+                Destroy(collision.gameObject);
+                Cherry += 1;
+                CherryNum.text = Cherry.ToString();
+            
+        }
+        if (collision.tag == "Collection2")
+        {
+                anim.SetTrigger("disappear");
+                Destroy(collision.gameObject);
+                Gem += 1;
+                GemNum.text = Gem.ToString();
+            
+        }
+    }
 
+    //消灭敌人
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+          Enemy_Frog frog = collision.gameObject.GetComponent<Enemy_Frog>();
+          if (anim.GetBool("falling"))
+             {
+                frog.JumpOn();
+                rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+                anim.SetBool("jumping", true);
+             }
+          else if(transform.position.x < collision.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(-10, rb.velocity.y);
+                isHurt = true;
+            }
+           else if (transform.position.x > collision.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(10, rb.velocity.y);
+                isHurt = true;
+            }
+        }
+    }
 }
