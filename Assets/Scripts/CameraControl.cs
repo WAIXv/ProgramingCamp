429742259f5b00e022d99e77d588f9802868f00b
蓟北门 <64835430+Patroned@users.noreply.github.com>
2022-11-21 17:16:49 +0800

    Revert "Revert "第二周作业""
    This reverts commit 651c2ab7fbd9548c214b917d8088696bcdac2ab8.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,10 +5,4 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     [SerializeField] private Transform player;
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(player.position.x, player.position.y, -10f);
-    }
 }
